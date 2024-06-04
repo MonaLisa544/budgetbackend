cb761502f338596205aeff8c7200a336e83a2a58
khongro <xongorzul54@gmail.com>
2024-06-04 14:40:53 +0800

    data folder create

--- db/fixtures/categories.seeds.rb
+++ db/fixtures/categories.seeds.rb
@@ -1,10 +1,10 @@
 
 require 'csv'
 
-csv_file = Rails.root.join("db/fixtures/data(1).csv")
+csv_file = Rails.root.join("db/fixtures/data/category.csv")
 
 CSV.foreach(csv_file, headers: true) do |row|
-  Category.seed do |s|
+  Category.seed_once(:id) do |s|
     s.name = row['name']
   end
 end
