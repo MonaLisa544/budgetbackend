aba45595c30d4ccbb646a694c9f35672ea25df3f
tuguldurchimeg <tgldrchmg0730@gmail.com>
2024-06-06 11:43:00 +0800

    session_controller edit

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,17 @@ class Users::SessionsController < Devise::SessionsController
   respond_to :json
   private
   def respond_with(resource, _opts = {})
-    render json: { message: 'Logged.' }, status: :ok
+  if resource
+    # current_user is logged in successfully
+    render json: {
+      user: current_user.as_json
+    }, status: 200
+  else
+    # current_user is not logged in successfully
+    render json: {
+      messages: ["Invalid Email or Password."],
+    }, status: 422
+  end
   end
   def respond_to_on_destroy
     current_user ? log_out_success : log_out_failure
