f6098731d7ef6f5be7ab36a1ad9a24c81469c521
tuguldurchimeg <tgldrchmg0730@gmail.com>
2024-06-13 13:53:26 +0800

    fix conflict

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -40,7 +40,6 @@ class Api::V1::CategoriesController < ApplicationController
     end
 
     def destroy
-        @category.update(delete_flag: true)
         render json: CategorySerializer.new(@category).serialized_json
     end
 
